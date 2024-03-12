68cd1bb0f2424e434d08d51ff4dc7519cb1aef1a
PtrKlus <iejan.massart@gmail.com>
2024-03-12 23:35:06 +0100

    initial map

--- script.js
+++ script.js
@@ -56,3 +56,96 @@ document.getElementById("startBtn4").addEventListener("click", function () {
     }
   });
 });
+upsArray = ["up1", "up2", "up3", "up4", "up5", "up6"];
+downsArray = ["down1", "down2", "down3", "down4", "down5", "down6"];
+lockArray = ["lock1", "lock2", "lock3", "lock4", "lock5", "lock6"];
+awnserArray = [];
+
+upsArray.forEach(function (button, i) {
+  document.getElementById(button).addEventListener("click", function (event) {
+    var element = document.getElementById(
+      "lock" + event.srcElement.id.charAt(2)
+    ).firstChild;
+    var number = parseInt(element.innerHTML);
+    if (number < 9) {
+      number++;
+    } else {
+      number = 0;
+    }
+    element.innerHTML = number;
+  });
+});
+downsArray.forEach(function (button, i) {
+  document.getElementById(button).addEventListener("click", function (event) {
+    var element = document.getElementById(
+      "lock" + event.srcElement.id.charAt(4)
+    ).firstChild;
+    var number = parseInt(element.innerHTML);
+    if (number > 0) {
+      number--;
+    } else {
+      number = 9;
+    }
+    element.innerHTML = number;
+  });
+});
+
+document
+  .getElementById("lockImage")
+  .addEventListener("click", function (event) {
+    awnserArray = [];
+    lockArray.forEach(function (button, i) {
+      awnserArray.push(document.getElementById(button).firstChild.innerHTML);
+    });
+
+    if (arrayEquals(awnserArray, ["1", "3", "1", "2", "2", "3"])) {
+      $("#date").hide();
+      $("#map").show();
+    }
+  });
+
+function arrayEquals(a, b) {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+}
+
+// Set up the SVG
+const svg = d3.select("#mapsvg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
+
+console.log("🚀 ~ svg:", svg);
+// Load Belgium GeoJSON data
+d3.json(
+  "https://raw.githubusercontent.com/bmesuere/belgium-topojson/master/belgium.json"
+).then(function (topojsondata) {
+  const geojson = topojson.feature(
+    topojsondata,
+    topojsondata.objects.municipalities
+  );
+  // Create a projection
+  const projection = d3.geoMercator().fitSize([width, height], geojson);
+  console.log("🚀 ~ projection:", projection);
+
+  // Create a path generator
+  const path = d3.geoPath().projection(projection);
+  console.log("🚀 ~ path:", path);
+
+  // Render the map
+  svg
+    .selectAll("path")
+    .data(geojson.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .on("mouseover", function () {
+      d3.select(this).attr("fill", "#f00"); // Change color on hover
+    })
+    .on("mouseout", function () {
+      d3.select(this).attr("fill", "#ccc"); // Reset color on mouseout
+    });
+});
